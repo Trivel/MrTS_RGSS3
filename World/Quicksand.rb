ab71afb729b77df4c601d5a6848f75ab08c8f7dc
Mr Trivel <gardauskas.a@gmail.com>
2014-10-29 12:32:39 +0200

    Quicksand v1.2

--- World/Quicksand.rb
+++ World/Quicksand.rb
@@ -3,11 +3,12 @@
 # )--     AUTHOR:     Mr Trivel                                              --(
 # )--     NAME:       Quicksand                                              --(
 # )--     CREATED:    2014-10-27                                             --(
-# )--     VERSION:    1.1                                                    --(
+# )--     VERSION:    1.2                                                    --(
 #===============================================================================
 # )--                         VERSION HISTORY                                --(
 # )--  1.0 - Initial script.                                                 --(
 # )--  1.1 - Player is slowed down while in quicksand.                       --(
+# )--  1.2 - Multiple quicksand tiles.                                       --(
 #===============================================================================
 # )--                          DESCRIPTION                                   --(
 # )--  Allows the developer to set which Terrain Tags are quicksand.         --(
@@ -30,42 +31,28 @@
 # )-- Module: Quicksand                                                      --(
 # )---------------------------------------------------------------------=======(
 module Quicksand
-  # )--------------------------------------------------------------------------(
-  # )--  Set which Terrain Tags are quicksand. E.g. [2, 3, 5]                --(
-  # )--  Best effect is if those tiles are set to Bush, too.                 --(
-  # )--------------------------------------------------------------------------(
-  QUICKSAND_TAGS = [1]
   
-  # )--------------------------------------------------------------------------(
-  # )--  Speed of player sinking. Default - 0.1 pixel per frame.             --(
-  # )--------------------------------------------------------------------------(
-  SINKING_SPEED = 0.1
-  
-  # )--------------------------------------------------------------------------(
-  # )--  Speed of player moving in quicksand the further he sinks.           --(
-  # )--------------------------------------------------------------------------(
-  SLOW_DOWN = 0.05
-  MAX_SLOW_DOWN = 0.5 #0.5 - 50%, #1.0 - no slowdown
-  
-  # )--------------------------------------------------------------------------(
-  # )--  If DEATH is true and player sinks in completely, player gets a game --(
-  # )--  over screen.                                                        --(
-  # )--  DEATH_SINK - required sunk pixels to get game over screen. By       --(
-  # )--  default player sprite is 32 pixels.                                 --(
-  # )--------------------------------------------------------------------------(
-  DEATH = true
-  DEATH_SINK = 32
-  
-  # 
-  # )--------------------------------------------------------------------------(
-  # )--  If DEATH is set to false, set the max possible sink.                --(
-  # )--------------------------------------------------------------------------(
-  MAX_SINK = 16
-  
-  # )--------------------------------------------------------------------------(
-  # )--  Can player Dash in quicksand?                                       --(
-  # )--------------------------------------------------------------------------(
-  DASH = false
+  Quicksands = {
+    # Quicksand
+    2 => {  :sinking_speed => 0.1,  # Sinking Speed in pixels per frame
+            :slow_down => 0.05,     # Movement slow-down by %
+            :max_slow_down => 0.5,  # Max slowdown in %
+            :death => true,         # Death after sinking?
+            :death_sink => 32,      # Pixels for death, VX Ace default is 32px
+            :max_sink => 32,        # Max sink possible
+            :dash => false          # Can dash in quicksand?
+         },
+    # Snow
+    3 => {  :sinking_speed => 0.1,
+            :slow_down => 0.04,
+            :max_slow_down => 0.5,
+            :death => false,
+            :death_sink => 32,
+            :max_sink => "$game_party.has_item?($data_items[1]) ? 4 : 8", # if party has item 1, make max sink 4, else it's 8
+            :dash => true
+         }
+    
+  }
 end
 
 # )=======---------------------------------------------------------------------(
@@ -101,14 +88,63 @@ class Game_Player < Game_Character
   # )--  New Method: in_quicksand?                                           --(
   # )--------------------------------------------------------------------------(
   def in_quicksand?
-    QUICKSAND_TAGS.include?(terrain_tag)
+    Quicksands[terrain_tag]
   end
+  
+  # )--------------------------------------------------------------------------(
+  # )--  New Method: qcks_can_dash?                                          --(
+  # )--------------------------------------------------------------------------(
+  def qcks_can_dash?
+    Quicksands[terrain_tag][:dash]
+  end
+  
+  # )--------------------------------------------------------------------------(
+  # )--  New Method: qcks_sink_speed                                         --(
+  # )--------------------------------------------------------------------------(
+  def qcks_sink_speed
+    Quicksands[terrain_tag][:sinking_speed]
+  end
+  
+  # )--------------------------------------------------------------------------(
+  # )--  New Method: qcks_max_sink                                           --(
+  # )--------------------------------------------------------------------------(
+  def qcks_max_sink
+    Quicksands[terrain_tag][:max_sink]
+  end
+  
+  # )--------------------------------------------------------------------------(
+  # )--  New Method: qcks_death_sink                                         --(
+  # )--------------------------------------------------------------------------(
+  def qcks_death_sink
+    Quicksands[terrain_tag][:death_sink]
+  end
+  
+  # )--------------------------------------------------------------------------(
+  # )--  New Method: qcks_death                                              --(
+  # )--------------------------------------------------------------------------(
+  def qcks_death
+    Quicksands[terrain_tag][:death]
+  end
+  
+  # )--------------------------------------------------------------------------(
+  # )--  New Method: qcks_slow_down                                          --(
+  # )--------------------------------------------------------------------------(
+  def qcks_slow_down
+    Quicksands[terrain_tag][:slow_down]
+  end
+  
+  # )--------------------------------------------------------------------------(
+  # )--  New Method: qcks_max_slow_down                                      --(
+  # )--------------------------------------------------------------------------(
+  def qcks_max_slow_down
+    Quicksands[terrain_tag][:max_slow_down]    
+  end  
   
   # )--------------------------------------------------------------------------(
   # )--  Alias: dash?                                                        --(
   # )--------------------------------------------------------------------------(
   def dash?
-    return false if in_quicksand? && !DASH
+    return false if in_quicksand? && !qcks_can_dash?
     mrts_qcksnd_dash?
   end
   
@@ -125,9 +161,10 @@ class Game_Player < Game_Character
   # )--------------------------------------------------------------------------(
   def update_sinking
     if in_quicksand?
-      @sunk += SINKING_SPEED
-      @sunk = MAX_SINK.to_f if @sunk > MAX_SINK.to_f && !DEATH
-      SceneManager.goto(Scene_Gameover) if @sunk >= DEATH_SINK.to_f
+      @sunk += qcks_sink_speed
+      max_sink = eval(qcks_max_sink.to_s).to_f
+      @sunk = max_sink if @sunk > max_sink
+      SceneManager.goto(Scene_Gameover) if @sunk >= qcks_death_sink.to_f && qcks_death
     else
       @sunk -= 8.0 if @sunk > 0
       @sunk = 0 if @sunk < 0
@@ -151,7 +188,7 @@ class Game_Player < Game_Character
     dy = (d == 2 ? 1 : d == 8 ? -1 : 0)
     new_x = @x + dx
     new_y = @y + dy
-    if in_quicksand? && ! QUICKSAND_TAGS.include?($game_map.terrain_tag(new_x, new_y)) && @move_succeed
+    if in_quicksand? && $game_map.terrain_tag(new_x, new_y) != terrain_tag && @move_succeed
       jump(dx, dy)
     else
       mrts_qcksnd_move_straight(d, turn_ok = true)
@@ -163,7 +200,7 @@ class Game_Player < Game_Character
   # )--------------------------------------------------------------------------(
   def update_move
     if in_quicksand?
-      slowdown = distance_per_frame * [MAX_SLOW_DOWN, (1 - @sunk*SLOW_DOWN)].max
+      slowdown = distance_per_frame * [qcks_max_slow_down, (1 - @sunk*qcks_slow_down)].max
       @real_x = [@real_x - slowdown, @x].max if @x < @real_x
       @real_x = [@real_x + slowdown, @x].min if @x > @real_x
       @real_y = [@real_y - slowdown, @y].max if @y < @real_y
