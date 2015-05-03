6eb4e5f4c0c2f13ad9265e45141776296fc00dbe
Mr Trivel <gardauskas.a@gmail.com>
2015-05-03 04:15:33 +0300

    New features
    Multiple drops possible.
    Variable "drops".

--- Battle/Conditional_Drops.rb
+++ Battle/Conditional_Drops.rb
@@ -3,15 +3,21 @@
 # )--     AUTHOR:     Mr Trivel                                              --(
 # )--     NAME:       Conditional Drops                                      --(
 # )--     CREATED:    2014-10-03                                             --(
-# )--     VERSION:    1.0                                                    --(
+# )--     VERSION:    1.1                                                    --(
 #===============================================================================
 # )--                         VERSION HISTORY                                --(
 # )--  1.0 - Initial script.                                                 --(
 # )--  1.0a - Fix when starting battle with enemis without conditional drops.--(
+# )--  1.1 - Multiple conditional drops are now allowed on same enemy.       --(
+# )--        Variable increase/decrease is now a valid drop.                 --(
 #===============================================================================
 # )--                          DESCRIPTION                                   --(
-# )--  Allows enemies to drop an extra item if a condition is fulfilled like --(
+# )--  Allows enemies to drop extra items if a condition is fulfilled like   --(
 # )--  killing it with an element which the enemy is strong to.              --(
+# )--                                                                        --(
+# )--  Also allows enemies to increase variables on their deaths if condition--(
+# )--  is fulfilled. Good for exterminate X monsters quests or variables     --(
+# )--  which count enemies killed for achievements and other things.         --(
 # )--                                                                        --(
 # )--  Idea is from from Etrian Odyssey games where monsters could drop an   --(
 # )--  extra item when a specific is fulfilled.                              --(
@@ -23,6 +29,10 @@
 # )--                    X - w, a, i - w - weapon, a - armor, i - item       --(
 # )--                    Y - item number                                     --(
 # )--                    Example: <c_drop: a 15> - 15th armor is the drop.   --(
+# )--                                                                        --(
+# )--                    For variable drop you'd use                         --(
+# )--                    <c_drop: variable_id number>                        --(
+# )--                    E.g. <c_drop: 23 1>                                 --(
 # )--                                                                        --(
 # )--    <c_chance X%> - What's the chance of getting conditional drop.      --(
 # )--                    Example: <c_chance 77.7%>                           --(
@@ -60,6 +70,14 @@
 # )--  <c_formula>                                                           --(
 # )--  $game_switch[15] == true                                              --(
 # )--  <\c_formula>                                                          --(
+# )--                                                                        --(
+# )--  #3 variable drop example:                                             --(
+# )--  <c_drop: 23 1>                                                        --(
+# )--  <c_chance: 100%>                                                      --(
+# )--  <c_type: custom>                                                      --(
+# )--  <c_formula>                                                           --(
+# )--  $game_switches[55]                                                    --(
+# )--  <\c_formula>                                                          --(
 #===============================================================================
 # )--                          LICENSE INFO                                  --(
 # )--  Free for non-commercial & commercial games if credit was given to     --(
@@ -84,34 +102,54 @@ class Game_Enemy < Game_Battler
   # )--------------------------------------------------------------------------(
   def initialize(index, enemy_id)
     mrts_conditional_drops_initialize(index, enemy_id) 
-    return unless enemy.conditional_drop
-    @conditional_drop_fulfilled = false
-    @conditional_drop = enemy.conditional_drop
-    data = enemy.conditional_drop_type
-    @conditional_type = data[0]
-    @conditional_type_data = data[1]
-    @conditional_chance = enemy.conditional_drop_chance
-    @conditional_custom = enemy.conditional_formula
+    return unless enemy.conditional_drops && enemy.conditional_drops.size > 0
+    
+    @conditional_drop_fulfilled = []
+    @conditional_drop = []
+    @conditional_type = []
+    @conditional_type_data = []
+    @conditional_chance = []
+    @conditional_custom = []
+    
+    i = 0
+    enemy.conditional_drops.each { |drop|
+      @conditional_drop_fulfilled[i] = false
+      @conditional_drop[i] = drop.conditional_drop
+      data = drop.conditional_drop_type
+      @conditional_type[i] = data[0]
+      @conditional_type_data[i] = data[1]
+      @conditional_chance[i] = drop.conditional_drop_chance
+      @conditional_custom[i] = drop.conditional_formula
+      i += 1
+    }
   end
   
   # )--------------------------------------------------------------------------(
   # )-- Alias: make_drop_items                                               --(
   # )--------------------------------------------------------------------------(
   def make_drop_items
-    return mrts_conditional_drops_make_drop_items unless @conditional_drop_fulfilled
+    return mrts_conditional_drops_make_drop_items unless @conditional_drop_fulfilled.any? { |f| f }
     drops = mrts_conditional_drops_make_drop_items
-    rolled = rand <= @conditional_chance / 100.0
-    return drops unless rolled
-    item = nil
-    case @conditional_drop[0]
-    when "w"
-      item = $data_weapons[@conditional_drop[1]]
-    when "a"
-      item = $data_armors[@conditional_drop[1]]
-    when "i"
-      item = $data_items[@conditional_drop[1]]
-    end
-    drops.push(item)
+    @conditional_drop_fulfilled.size.times { |i|    
+      rolled = rand <= @conditional_chance[i] / 100.0
+      next unless rolled && @conditional_drop_fulfilled[i]
+      item = nil
+      case @conditional_drop[i][0]
+      when "w"
+        item = $data_weapons[@conditional_drop[i][1]]
+      when "a"
+        item = $data_armors[@conditional_drop[i][1]]
+      when "i"
+        item = $data_items[@conditional_drop[i][1]]
+      else
+        if $game_variables[@conditional_drop[i][0].to_i]
+          $game_variables[@conditional_drop[i][0].to_i] += @conditional_drop[i][1]
+        else
+          $game_variables[@conditional_drop[i][0].to_i] = @conditional_drop[i][1]
+        end      
+      end
+      drops.push(item) if item
+    }
     drops
   end
   
@@ -120,14 +158,16 @@ class Game_Enemy < Game_Battler
   # )--------------------------------------------------------------------------(
   def fulfill_conditional
     return unless @conditional_drop
-    case @conditional_type
-    when :elemental
-      @conditional_drop_fulfilled = @conditional_type_data == @result.element
-    when :state
-      @conditional_drop_fulfilled = state?(@conditional_type_data)
-    when :custom
-      @conditional_drop_fulfilled = eval(@conditional_custom)
-    end
+    @conditional_drop.size.times { |i|
+      case @conditional_type[i]
+      when :elemental
+        @conditional_drop_fulfilled[i] = @conditional_type_data[i] == @result.element
+      when :state
+        @conditional_drop_fulfilled[i] = state?(@conditional_type_data[i])
+      when :custom
+        @conditional_drop_fulfilled[i] = eval(@conditional_custom[i])
+      end
+    }
   end
   
   # )--------------------------------------------------------------------------(
@@ -180,36 +220,58 @@ class RPG::Enemy < RPG::BaseItem
   # )--------------------------------------------------------------------------(
   # )--  Public Instance Variables                                           --(
   # )--------------------------------------------------------------------------(
+  attr_accessor :conditional_drops
+  
+  # )--------------------------------------------------------------------------(
+  # )-- Method: parse_conditional_drops                                      --(
+  # )--------------------------------------------------------------------------(
+  def parse_conditional_drops
+    
+    @conditional_drops = []
+    
+    tmp = nil
+    
+    note.split(/[\r\n]+/).each do |l|
+      case l
+      when /<c_drop:[ ]*(.*)[ ](-*\d+)>/i
+        tmp = CDrop.new
+        @conditional_drops.push(tmp)
+        tmp.conditional_drop = [$1, $2.to_i]
+      when /<c_type:[ ]*([a-z]*)[ ]*(\d+)*>/i
+        tmp.conditional_drop_type = [$1.to_sym, $2.to_i]
+      when /<c_chance:[ ]*(\d+)[%]*>/i
+        tmp.conditional_drop_chance = $1.to_f
+      when /<c_formula>/i
+        @c_drop_formula = true
+      when /<\\c_formula>/i
+        @c_drop_formula = false
+      else
+        tmp.conditional_formula += l if @c_drop_formula
+      end
+    end
+  end
+end
+
+# )=======---------------------------------------------------------------------(
+# )-- Class: CDrop                                                           --(
+# )---------------------------------------------------------------------=======(
+class CDrop
+  # )--------------------------------------------------------------------------(
+  # )--  Public Instance Variables                                           --(
+  # )--------------------------------------------------------------------------(
   attr_accessor :conditional_drop
   attr_accessor :conditional_drop_type
   attr_accessor :conditional_drop_chance
   attr_accessor :conditional_formula
   
   # )--------------------------------------------------------------------------(
-  # )-- Method: parse_conditional_drops                                      --(
+  # )--  Method: initialize                                                  --(
   # )--------------------------------------------------------------------------(
-  def parse_conditional_drops
+  def initialize
     @conditional_drop = nil
     @conditional_drop_type = nil
     @conditional_drop_chance = 100
     @conditional_formula = ""
-    
-    note.split(/[\r\n]+/).each do |l|
-      case l
-      when /<c_drop:[ ]*(.)[ ](\d+)>/i
-        @conditional_drop = [$1, $2.to_i]
-      when /<c_type:[ ]*([a-z]*)[ ]*(\d+)*>/i
-        @conditional_drop_type = [$1.to_sym, $2.to_i]
-      when /<c_chance:[ ]*(\d+)[%]*>/i
-        @conditional_drop_chance = $1.to_f
-      when /<c_formula>/i
-        @c_drop_formula = true
-      when /<\\c_formula>/i
-        @c_drop_formula = false
-      else
-        @conditional_formula += l if @c_drop_formula
-      end
-    end
   end
 end
 
